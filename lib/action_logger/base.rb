48dad92787106acea11426c2c3d9d8c05c1bb1b4
John Tjanaka <zonecheung@gmail.com>
2011-07-02 02:59:58 -0500

    Fix the log_request method to allow passing parameter from class to instance.

--- lib/action_logger/base.rb
+++ lib/action_logger/base.rb
@@ -1,37 +1,35 @@
 module ActionLogger
-  class Base
+  module Base
     def self.included(base)
       base.send :extend, ClassMethods
     end
    
     module ClassMethods
+
       def log_request(options = {})
-        if (options[:only].blank? || convert_options(options[:only]) { |x| x.to_s }.include?(params[:action])) &&
-           (options[:except].blank? || ! convert_options(options[:except]) { |x| x.to_s }.include?(params[:action])) &&
-           (options[:method].blank? || convert_options(options[:method]) { |x| x.to_s.uppercase }.include?(request.method))
-          before_filter :log_current_request_action
-        end
+        cattr_accessor :only, :except, :method
+
+        self.only   = ([options[:only]].flatten.compact || []).map(&:to_s)
+        self.except = ([options[:except]].flatten.compact || []).map(&:to_s)
+        self.method = ([options[:method]].flatten.compact || []).map { |x| x.to_s.upcase }
+
+        before_filter :log_current_request_action
       end
 
-      private
-
-      def convert_options(options, &block)
-        if options.is_a?(Array)
-          options.map(&block)
-        else
-          [block.call(options)]
-        end
-      end
     end
 
     def log_current_request_action
-      ActionLog.create(:user_id            => current_user && current_user.id,
-                       :controller         => params[:controller],
-                       :action             => params[:action],
-                       :request_parameters => params,
-                       :request_url        => request.url,
-                       :request_method     => request.method.to_s,
-                       :remote_ip          => request.remote_ip)
+      if (self.class.only.blank? || self.class.only.include?(params[:action])) &&
+         (self.class.except.blank? || ! self.class.except.include?(params[:action])) &&
+         (self.class.method.blank? || self.class.method.include?(request.method))
+        ActionLog.create(:user_id            => current_user && current_user.id,
+                         :controller         => params[:controller],
+                         :action             => params[:action],
+                         :request_parameters => params,
+                         :request_url        => request.url,
+                         :request_method     => request.method.to_s,
+                         :remote_ip          => request.remote_ip)
+      end
     end
   end
 end
