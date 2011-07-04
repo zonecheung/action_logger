931f7ebbb00ca9f0052c49d4b56733c960d2d06b
John Tjanaka <zonecheung@gmail.com>
2011-07-04 18:46:04 -0500

    Remove unnecessary files from the gem.

--- config/environment.rb
+++
@@ -1,9 +0,0 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-ActionLogger::Application.initialize!
-
-# NOTE: This has nothing to do with the code.
-# It's for trickering Rails bundle in TextMate to choose a HAML file when pressed alt-cmd-arrow_down.
-# config.gem 'haml'
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  resources :action_logs, :only => [:index, :show, :destroy] do
-    delete :destroy_selected, :on => :collection
-  end
-end
-
