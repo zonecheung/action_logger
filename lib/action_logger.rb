bacfa4c6cd62f58668dfa339044935f77c8c5085
John Tjanaka <zonecheung@gmail.com>
2011-07-04 18:34:31 -0500

    Revert the engine settings back

--- lib/action_logger.rb
+++ lib/action_logger.rb
@@ -4,16 +4,16 @@ require "action_logger/base"
 module ActionLogger
   # Our host application root path
   # We set this when the engine is initialized
-  mattr_accessor :app_root
+  #mattr_accessor :app_root
 
   # Yield self on setup for nice config blocks
-  def self.setup
-    yield self
-  end
-  #class Engine < Rails::Engine
-  #end if defined?(Rails) && Rails::VERSION::MAJOR == 3
+  #def self.setup
+    #yield self
+  #end
+  class Engine < Rails::Engine
+  end if defined?(Rails) && Rails::VERSION::MAJOR == 3
 end
 
 # Require our engine
-require "action_logger/engine"
+#require "action_logger/engine"
 
