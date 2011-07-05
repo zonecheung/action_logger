9ce41162e74d317d6d23faaa04a4efd65208b7ce
John Tjanaka <zonecheung@gmail.com>
2011-07-05 03:23:04 -0500

    Some UI fixes and remove script/ from repository.

--- lib/action_logger.rb
+++ lib/action_logger.rb
@@ -2,6 +2,9 @@ require "action_logger/base"
 
 module ActionLogger
   class Engine < Rails::Engine
+    initializer "static assets" do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
   end if defined?(Rails) && Rails::VERSION::MAJOR == 3
 end
 
